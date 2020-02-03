ce283d8743b90c96396ac627957bb2f6cc3cb87f
Soliman Mohammad <solimohammad@hotmail.com>
2020-02-03 11:35:43 -0500

    Deuxième commit la création de la classe de base.

---
+++ TP1/src/TP1/CV.java
@@ -0,0 +1,24 @@
+package TP1;
+
+public class CV {
+
+	String nom;
+	String prenom;
+	String formation;
+	int experience;
+	String[] competences;
+	String attentes;
+
+	public void affiche() {
+		
+	}
+
+	public CV(String Nom, String Prenom, String Formation, int Experience, String[] Competences, String Attentes) {
+		
+	}
+
+	public static void main(String[] args) {
+
+	
+	}
+}
